1df8f7eb77d00c093000bee7312a636a24700af6
Sayem Mohammed Al-Mahdi <sayemmohammedal-mahdi@Sayems-MacBook-Pro.local>
2022-12-28 19:10:26 +0600

    Removed some files from src folder and did practice about Components, Templates, Dynamic Values in Templates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = "Welcome to the React Blog App"
+  const like = 50;
+  const link = "http://google.com"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{ title }</h1>
+        <p>Likes { like } times</p>
+        <a href={link}>Google Site</a>
+      </div>
     </div>
   );
 }
