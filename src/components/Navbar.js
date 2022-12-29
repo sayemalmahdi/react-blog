9cee69dc69f4bf9f9f008e34fb1b00ce9b223593
Sayem Mohammed Al-Mahdi <sayemmohammedal-mahdi@Sayems-MacBook-Pro.local>
2022-12-29 20:11:26 +0600

    Add Some CSS into the index.css, deleted App.css, Adding inline Css into Navbar Components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,11 @@ const Navbar = () => {
             <h1>The React Blog</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create">New Blog</a>
+                <a href="/create" style={{
+                  color: "white",
+                  backgroundColor: '#f1356d',
+                  borderRadius: '8px',
+                }}>New Blog</a>
             </div>
         </nav>
     </div>
