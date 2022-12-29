ecd04e819ac21c2a7b2840df1faacc4f30aa40ea
Sayem Mohammed Al-Mahdi <sayemmohammedal-mahdi@Sayems-MacBook-Pro.local>
2022-12-29 20:37:20 +0600

    Practicing Click Events into the Home Components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,22 @@
 const Home = () => {
     
-    const title = "Home Page From Home Components"
-    const like = 50;
-    const link = "http://google.com"
+  const title = "Home Page"
+  const handleClick = () => {
+    console.log ('Helloo world !');
+  }
+  const handleClickAgain = (name) => {
+    console.log ('Hello ' + name)
+  }
+  const handleClickE = (e) => {
+    console.log (e);
+  }
+
   return (
     <div className="home">
-        <h1>{ title }</h1>
-        <p>Likes { like } times</p>
-        <a href={link}>Google Site</a>
+        <h2>{ title }</h2> <br/>
+        <button onClick={handleClick}>Click Me</button> &nbsp;
+        <button onClick={() => handleClickAgain('Sayem')}>Click Me Again</button> &nbsp;
+        <button onClick={handleClickE}>Click Me to see All Event Staff on Console</button> 
     </div>
   )
 }
