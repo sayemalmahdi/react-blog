36b58e23e07d1d4b64f09ffce269825c06dc5e29
Sayem Mohammed Al-Mahdi <sayemmohammedal-mahdi@Sayems-MacBook-Pro.local>
2023-01-09 19:25:08 +0600

    Using JSON Server

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,18 +18,11 @@ const Home = () => {
 
   useEffect(() =>{
     console.log('From UseEffect Hooks');
-    // console.log(blogs);
-  }, [name]);
+  }, []);
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-      <button onClick={() => setName('Mahdi')}>Chande Name</button>
-      <p>{name}</p>
-
-      {/* Showing only Mario's Blog */}
-      {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'mario' )} title="Mario's Blog" handleDelete={handleDelete}/> */}
-
     </div> 
   )
 }
