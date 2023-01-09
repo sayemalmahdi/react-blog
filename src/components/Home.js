732cb4ba36361c58e6fd89ff847d1c36bdd88510
Sayem Mohammed Al-Mahdi <sayemmohammedal-mahdi@Sayems-MacBook-Pro.local>
2023-01-09 20:11:27 +0600

    Handling Fetch Errors

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,25 +4,35 @@ import BlogList from "./BlogList";
 const Home = () => {
     
   const [blogs, setBlogs] = useState(null);
-  const [name, setName] = useState('Sayem')
   const [isPending, setIsPending] = useState(true); 
+  const [error, setError] = useState(null);
 
-  useEffect(() =>{
+  useEffect(() => {
     setTimeout(() => {
       fetch('http://localhost:8000/blogs')
       .then(res => {
+        if (!res.ok) { // error coming back from server
+          throw Error('could not fetch the data for that resource');
+        } 
         return res.json();
       })
       .then(data => {
         setIsPending(false);
         setBlogs(data);
+        setError(null);
+      })
+      .catch(err => {
+        // auto catches network / connection error
+        setIsPending(false);
+        setError(err.message);
       })
     }, 1000);
-  }, []);
+  }, [])
 
   return (
     <div className="home">
-       {isPending && <div>Loading ...</div>}
+      { error && <div>{ error }</div> }
+      {isPending && <div>Loading ...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs"/> }
     </div> 
   )
