56b47c26e5e6f1b8ce0c1fdfa189faa798c8c60d
Sayem Mohammed Al-Mahdi <sayemmohammedal-mahdi@Sayems-MacBook-Pro.local>
2023-01-09 18:35:19 +0600

    Functions as Props

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,7 +1,6 @@
-const BlogList = ({blogs, title}) => {
+const BlogList = ({blogs, title, handleDelete }) => {
     // const blogs = props.blogs;
     // const title = props.title;
-
   return (
     <div className="blog-list">
         <h2>{title}</h2>
@@ -9,6 +8,7 @@ const BlogList = ({blogs, title}) => {
         <div className="blog-preview" key={blog.id} >
           <h2>{ blog.title }</h2>
           <p>Written by { blog.author }</p>
+          <button onClick={() => handleDelete(blog.id)}>Delete Button</button>
         </div>
       ))}
     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -9,10 +9,18 @@ const Home = () => {
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ])
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+  }
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" />
-      <BlogList blogs={blogs.filter((blog) => blog.author === 'mario' )} title="Mario's Blog" />
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+
+      {/* Showing only Mario's Blog */}
+      {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'mario' )} title="Mario's Blog" handleDelete={handleDelete}/> */}
+
     </div> 
   )
 }
