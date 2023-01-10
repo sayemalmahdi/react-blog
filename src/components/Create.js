b6f35621aa812b0d89d7686401f5ae12e17440ed
Sayem Mohammed Al-Mahdi <sayemalmahdi@gmail.com>
2023-01-10 22:26:04 +0600

    Programmatic Redirects

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState(''); 
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('Sayem');
   const [isPending, setIsPending] = useState(false);
+  const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -15,7 +17,9 @@ const Create = () => {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(blog)
     }).then(() => {
-      console.log('new blog added');
+      // console.log('new blog added');
+      // history.go(-1);
+      history.push('/');
     })
   }
 
