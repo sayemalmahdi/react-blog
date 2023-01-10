794f7350e295f9ea41af5502ec9c86e90e8ef642
Sayem Mohammed Al-Mahdi <sayemalmahdi@gmail.com>
2023-01-10 21:01:35 +0600

    Route Parameters

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './components/Create';
+import BlogDetails from './components/BlogDetails';
 
 function App() {
   
@@ -16,6 +17,9 @@ function App() {
             </Route>
             <Route path={"/create"}>
               <Create />
+            </Route>
+            <Route path={"/blogs/:id"}>
+              <BlogDetails />
             </Route>
           </Switch>
         </div>
