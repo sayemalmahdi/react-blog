7faf730ecaa88be644447a5a474e4414015d0866
Sayem Mohammed Al-Mahdi <sayemalmahdi@gmail.com>
2023-01-10 21:37:47 +0600

    Controlled Inputs (forms)

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,10 +1,42 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState(''); 
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('Sayem');
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input 
+          type="text" 
+          required 
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog author:</label>
+        <select
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
+          <option value="Sayem">Sayem</option>
+          <option value="Mahdi">Mahdi</option>
+        </select>
+        <button>Add Blog</button>
+        <p>{title}</p>
+        <p>{body}</p>
+        <p>{author}</p>
+      </form>
     </div>
-  )
+  );
 }
-
-export default Create
  
+export default Create;
