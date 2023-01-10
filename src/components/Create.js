1c043c01eaca451f6e903075e6198e7b3ee30b00
Sayem Mohammed Al-Mahdi <sayemalmahdi@gmail.com>
2023-01-10 22:04:09 +0600

    Making a POST Request

--- src/components/Create.js
+++ src/components/Create.js
@@ -4,12 +4,19 @@ const Create = () => {
   const [title, setTitle] = useState(''); 
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('Sayem');
+  const [isPending, setIsPending] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author };
 
-    console.log(blog);
+    fetch('http://localhost:8000/blogs/', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog)
+    }).then(() => {
+      console.log('new blog added');
+    })
   }
 
   return (
@@ -37,10 +44,8 @@ const Create = () => {
           <option value="Sayem">Sayem</option>
           <option value="Mahdi">Mahdi</option>
         </select>
-        <button>Add Blog</button>
-        <p>{title}</p>
-        <p>{body}</p>
-        <p>{author}</p>
+        { !isPending && <button>Add Blog</button> }
+        { isPending && <button disabled>Adding Blog...</button> }
       </form>
     </div>
   );
